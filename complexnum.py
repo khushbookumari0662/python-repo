725595f4ac70286b469e3775fc38c89cf9b506ae
Khushboo Kumari <129657452+khushbookumari0662@users.noreply.github.com>
2026-03-04 11:01:47 +0530

    Create complexnum.py

---
+++ complexnum.py
@@ -0,0 +1,16 @@
+# complex numbers
+a = 3 + 4j
+b = 2 - 5j
+
+print("Complex number a ", a)
+print("Complex number b ", b)
+
+# Real and Imaginary parts
+print("Real part of a ", a.real)
+print("Imaginary part of a ", a.imag)
+
+# Addition
+print("Addition ", a + b)
+
+# Multiplication
+print("Multiplication ", a * b)
