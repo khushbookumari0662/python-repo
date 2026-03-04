c318a9d6310dbee919b9e076b6b82aed03f764a8
Khushboo Kumari <129657452+khushbookumari0662@users.noreply.github.com>
2026-03-04 14:28:54 +0530

    Create StringSlicing.py

---
+++ StringSlicing.py
@@ -0,0 +1,21 @@
+text = "KhushbooKumari"
+
+print("Original String:", text)
+
+# from index 0 to 5
+print("text[0:6] =", text[0:6])
+
+# from index 6 to end
+print("text[6:] =", text[6:])
+
+# from start to index 6
+print("text[:6] =", text[:6])
+
+# full string
+print("text[:] =", text[:])
+
+# with step
+print("text[::2] =", text[::2])
+
+# reverse string
+print("text[::-1] =", text[::-1])
