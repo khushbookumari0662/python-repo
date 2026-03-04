ea382d51da4299c3f1c745fb37074eb772850c3b
Khushboo Kumari <129657452+khushbookumari0662@users.noreply.github.com>
2026-03-04 10:52:47 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,43 @@
+# Integer
+a = 123
+print("Integer:", a, type(a))
+
+# Float
+b = 45.67
+print("Float:", b, type(b))
+
+# Complex
+c = 3 + 4j
+print("Complex:", c, type(c))
+
+# String
+name = "Khushboo"
+print("String:", name, type(name))
+
+# Boolean
+is_student = True
+print("Boolean:", is_student, type(is_student))
+
+# List
+numbers = [101, 202, 303]
+print("List:", numbers, type(numbers))
+
+# Tuple
+marks = (88, 92, 76)
+print("Tuple:", marks, type(marks))
+
+# Set
+unique_values = {10, 20, 30, 20}
+print("Set:", unique_values, type(unique_values))
+
+# Dictionary
+student = {
+    "name": "Khushboo",
+    "roll": 39,
+    "course": "MCA"
+}
+print("Dictionary:", student, type(student))
+
+# None Type
+x = None
+print("None Type:", x, type(x))
