5b7b9fa79ba6e242ca155216af90764ecc720cf8
Khushboo Kumari <129657452+khushbookumari0662@users.noreply.github.com>
2026-03-06 13:11:06 +0530

    Create help.py

---
+++ help.py
@@ -0,0 +1,9 @@
+#help() = Shows the documentation or description of any python object.
+#examples
+
+import math
+
+print(help(len))        
+print(help(math.sqrt))  
+print(help("if"))       
+help(str.upper)
